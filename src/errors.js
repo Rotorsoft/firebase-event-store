e36158af914f8384481cc7bd716560a8289122bb
Rotorsoft <41707007+Rotorsoft@users.noreply.github.com>
2018-09-07 15:26:49 -0400

    Delete errors.js

--- src/errors.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-module.exports = {
-  notImplemented: (method) => new Error(`not implemented: ${method}`),
-  invalidArguments: (args) => new Error(`invalid arguments: ${args}`),
-  missingArguments: (args) => new Error(`missing arguments: ${args}`),
-  concurrencyError: () => new Error('concurrency error'),
-  preconditionError: (description) => new Error(`precondition error: ${description}`)
-}
